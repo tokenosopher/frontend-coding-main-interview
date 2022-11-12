0a53122e7e9cb0777fa68e15f19d0c9c6e3c225a
NarcizzeN <47037769+NarcizzeN@users.noreply.github.com>
2022-11-12 15:07:34 +0000

    got it to work with the comments

--- pages/feed.tsx
+++ pages/feed.tsx
@@ -10,6 +10,7 @@ import Avatar from "@mui/material/Avatar";
 import IconButton, { IconButtonProps } from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 
 import {
   Favorite,
@@ -19,8 +20,9 @@ import {
 } from "@mui/icons-material";
 
 import { useEffect, useState } from "react";
-import { Container } from "@mui/material";
-const API_LINK = "https://jsonplaceholder.typicode.com/comments";
+import { Box, Container } from "@mui/material";
+const COMMENTS_API_LINK = "https://jsonplaceholder.typicode.com/comments";
+const POSTS_API_LINK = "https://jsonplaceholder.typicode.com/posts";
 
 type Comment = {
   postId: number;
@@ -32,15 +34,15 @@ type Comment = {
 
 type Post = {
   userId: number;
-    id: number;
-    title: string;
-    body: string;
-}
+  id: number;
+  title: string;
+  body: string;
+};
 
 type PageProps = {
-    comments: Comment[];
-    posts: Post[];
-}
+  comments: Comment[];
+  posts: Post[];
+};
 
 interface ExpandMoreProps extends IconButtonProps {
   expand: boolean;
@@ -57,111 +59,81 @@ const ExpandMore = styled((props: ExpandMoreProps) => {
   }),
 }));
 
-const Feed: NextPage = ({ posts, comments }: PageProps) => {
+const Feed: NextPage<PageProps> = ({ posts, comments }) => {
   const [expanded, setExpanded] = useState(false);
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
 
-  useEffect(() => {
-    console.log(comments);
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <Container>
-      <Card>
-        <CardHeader
-          avatar={
-            <Avatar sx={{ bgcolor: red[500] }} aria-label="comment">
-              R
-            </Avatar>
-          }
-          action={
-            <IconButton aria-label="settings">
-              <MoreVert />
-            </IconButton>
-          }
-          title="Shrimp and Chorizo Paella"
-          subheader="September 14, 2016"
-        />
-        <CardMedia
-          component="img"
-          height="194"
-          image="/static/images/cards/paella.jpg"
-          alt="Paella dish"
-        />
-        <CardContent>
-          <Typography variant="body2" color="text.secondary">
-            This impressive paella is a perfect party dish and a fun meal to
-            cook together with your guests. Add 1 cup of frozen peas along with
-            the mussels, if you like.
-          </Typography>
-        </CardContent>
-        <CardActions disableSpacing>
-          <IconButton aria-label="add to favorites">
-            <Favorite />
-          </IconButton>
-          <IconButton aria-label="share">
-            <Share />
-          </IconButton>
-          <ExpandMore
-            expand={expanded}
-            onClick={handleExpandClick}
-            aria-expanded={expanded}
-            aria-label="show more"
-          >
-            <ExpandMoreIcon />
-          </ExpandMore>
-        </CardActions>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
+      {posts.map((post) => (
+        <Card key={post.id}>
+          <CardHeader title={post.title} />
           <CardContent>
-            <Typography paragraph>Method:</Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron
-              and set aside for 10 minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet
-              over medium-high heat. Add chicken, shrimp and chorizo, and cook,
-              stirring occasionally until lightly browned, 6 to 8 minutes.
-              Transfer shrimp to a large plate and set aside, leaving chicken
-              and chorizo in the pan. Add pimentón, bay leaves, garlic,
-              tomatoes, onion, salt and pepper, and cook, stirring often until
-              thickened and fragrant, about 10 minutes. Add saffron broth and
-              remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes
-              and peppers, and cook without stirring, until most of the liquid
-              is absorbed, 15 to 18 minutes. Reduce heat to medium-low, add
-              reserved shrimp and mussels, tucking them down into the rice, and
-              cook again without stirring, until mussels have opened and rice is
-              just tender, 5 to 7 minutes more. (Discard any mussels that
-              don&apos;t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then
-              serve.
+            <Typography variant="body2" color="text.secondary">
+              {post.body}
             </Typography>
           </CardContent>
-        </Collapse>
-      </Card>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <Favorite />
+            </IconButton>
+            <IconButton aria-label="share">
+              <Share />
+            </IconButton>
+            <ExpandMore
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </ExpandMore>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Comments postId={post.id} comments={comments} />
+            </CardContent>
+          </Collapse>
+        </Card>
+      ))}
     </Container>
   );
 };
 
-export const getServerSideProps = async () => {
+const Comments = ({
+  postId,
+  comments,
+}: {
+  postId: number;
+  comments: Comment[];
+}) => {
+  const postComments = comments.filter((comment) => comment.postId === postId);
+  return (
+    <>
+      {postComments.map((comment) => (
+        <Typography variant="body2" color="text.secondary" key={comment.id}>
+          {comment.body}
+        </Typography>
+      ))}
+    </>
+  );
+};
 
+export const getServerSideProps: GetServerSideProps = async () => {
   const [postsResponse, commentsResponse] = await Promise.all([
-    fetch(API_LINK),
-    fetch(API_LINK),
-    ]);
+    fetch(POSTS_API_LINK),
+    fetch(COMMENTS_API_LINK),
+  ]);
 
   const [posts, comments] = await Promise.all([
     postsResponse.json(),
     commentsResponse.json(),
-    ]);
+  ]);
 
   return {
     props: {
